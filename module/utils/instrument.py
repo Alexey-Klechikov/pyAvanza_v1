6dde762728ccc51620aeeb5cb37e2aaa0c43f57d
Alexey Klechikov <alexey.klechikov@anyfin.com>
2022-06-15 23:16:58 +0200

    restored instrument.py file

---
+++ module/utils/instrument.py
@@ -0,0 +1,34 @@
+"""
+This module is operating 'instruments.json' file, that is responsible for storing BEAR/BULL instruments.
+"""
+
+
+import os
+import json
+import logging
+import yfinance as yf
+
+
+log = logging.getLogger("main.instruments")
+
+
+class Instrument:
+    def __init__(self):
+        trading_multiplier = 18
+        self.ids_dict = self.get_id(trading_multiplier)
+
+    def get_id(self, trading_multiplier):
+        log.info(
+            f"Getting instrument_ids from instruments.json for trading_multiplier={trading_multiplier}"
+        )
+
+        current_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+        with open(f"{current_dir}/instruments.json", "r") as f:
+            instruments_json = json.load(f)
+
+        instrument_ids_dict = {
+            "MONITORING": instruments_json['MONITORING'],
+            "TRADING": instruments_json['TRADING'][str(trading_multiplier)]}
+
+        return instrument_ids_dict
