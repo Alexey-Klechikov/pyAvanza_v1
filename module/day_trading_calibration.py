adc0b0ac36fec6dde6fbfad032dfbf63ec91ed4f
Alexey Klechikov <alexey.klechikov@anyfin.com>
2022-09-24 08:43:39 +0200

    * modified cell and buy prices in calibration to avarage instead of random

--- module/day_trading_calibration.py
+++ module/day_trading_calibration.py
@@ -1,4 +1,3 @@
-import random
 import logging
 import platform
 import traceback
@@ -38,7 +37,7 @@ class CalibrationOrder:
     def buy(self, row, index):
         self.on_balance = True
         self.price_buy
-        self.price_buy = random.uniform(row["High"], row["Low"]) * (
+        self.price_buy = ((row["Low"] + row["High"]) / 2) * (
             1.00015 if self.instrument == "BULL" else 0.99985
         )
         self.time_buy = index
@@ -72,7 +71,7 @@ class CalibrationOrder:
                 self.price_buy if self.price_buy is not None else take_profit_normalized
             )
 
-        self.price_sell = random.uniform(row["High"], row["Low"])
+        self.price_sell = (row["Low"] + row["High"]) / 2
 
         if (self.price_sell <= stop_loss_normalized and self.instrument == "BULL") or (
             self.price_sell >= stop_loss_normalized and self.instrument == "BEAR"
