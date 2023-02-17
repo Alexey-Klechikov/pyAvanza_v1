2f0eb08471b9cb63ccf4f8fc5f92756edb533886
Alexey Klechikov <alexey.klechikov@anyfin.com>
2023-02-17 18:44:01 +0100

    added limit_history_hours 4 to calibration

--- module/dt/calibration/main.py
+++ module/dt/calibration/main.py
@@ -539,6 +539,7 @@ class Calibration:
                 Cache.SKIP,
                 filter_strategies=False,
                 loaded_strategies=stored_strategies.get("use", []),
+                limit_history_hours=4,
             ),
             key=lambda s: s["profit"],
             reverse=True,
