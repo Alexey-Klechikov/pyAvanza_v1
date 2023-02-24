25476e931ce13bbc9df2f0a7a027d23d2b92b9be
Alexey Klechikov <alexey.klechikov@anyfin.com>
2023-02-24 11:13:37 +0100

    bugfix

--- auto_day_trading_calibration.py
+++ auto_day_trading_calibration.py
@@ -4,4 +4,4 @@ from module.utils import Logger
 if __name__ == "__main__":
     log = Logger(file_prefix="auto_day_trading_calibration")
 
-    run_day_trading_calibration(update=True, adjust=True, print_orders_history=False)
+    run_day_trading_calibration(update=True, adjust=True, show_orders=False)
